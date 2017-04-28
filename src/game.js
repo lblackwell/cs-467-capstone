aabdeea76479f3a1a30f101162e4da4fb73b1f27
Lucia Blackwell <lblackwe@udel.edu>
2017-04-28 11:24:15 -0700

    Added scrolling

--- src/game.js
+++ src/game.js
@@ -1,3 +1,6 @@
+var screenWidth = 800;
+var screenHeight = 600;
+
 Game = {
 	start: function() {
 		Crafty.init(800,600, document.getElementById('game'));
@@ -6,8 +9,13 @@ Game = {
 
 		Crafty.defineScene('HomeScreen', function() {
 			Crafty.e('2D, DOM, Text')
-				.attr({x: 100, y: 100, w: 100, h: 20})
-				.text('Press Enter to begin');
+				.attr({x: 0, y: 300, w: 800, h: 600})
+				.text('Press Enter to begin')
+				.textFont({ family: 'Trebuchet MS',
+							size: '30px',
+							weight: 'bold'})
+				.textColor('#373854')
+				.textAlign('center');
 			Crafty.e('Start, 2D, Canvas, Color, Solid')
 				.attr({x: 200, y: 200, w: 100, h: 40})
 				.bind('KeyDown', function(e) {
@@ -19,7 +27,7 @@ Game = {
 
 		Crafty.defineScene('World', function() {
 			// Player sprite
-	        Crafty.e('2D, DOM, Color, Twoway, Gravity')
+	        var player = Crafty.e('2D, DOM, Color, Twoway, Gravity')
 	        	// Initial position and size
 	      		.attr({x: 0, y: 0, w: 10, h: 50})
 	      		// Color of sprite (to be replaced)
@@ -28,7 +36,7 @@ Game = {
 	      		.twoway(200)
 	      		// Set platforms to stop falling player
 	      		.gravity('Platform')
-	      		.gravityConst(650)
+	      		.gravityConst(600)
 	      		// Bind spacebar to jump action
 	      		.jumper(300, [Crafty.keys.SPACE])
 	      		// Allow player to drop through platforms
@@ -40,7 +48,7 @@ Game = {
 	      		.bind('KeyUp', function(e) {
 	      			if(e.key == Crafty.keys.DOWN_ARROW) {
 	      				this.gravity = this.gravity('Platform');
-	      		}});
+	      			}});
 
 	      	// Platforms
 	      	Crafty.e('Platform, 2D, Canvas, Color')
@@ -59,9 +67,12 @@ Game = {
 	      		.attr({x: 170, y: 540, w: 100, h: 10})
 	      		.color('green');
 
-	      	Crafty.e('Platform, 2D, Canvas, Color')
-	      		.attr({x: 0, y: 590, w: 800, h: 10})
+	      	Crafty.e('Platform, 2D, Canvas, Color')  // Floor
+	      		.attr({x: -4000, y: 590, w: 8000, h: 10})
 	      		.color('green');
+
+	       	// Have camera follow player sprite
+	       	Crafty.viewport.follow(player, 0, 50);
 
       	});
 
